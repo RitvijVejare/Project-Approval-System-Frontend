3c6f9659b73500e0240ad9ad19a94056c2839089
kapish singh <skapish27@gmail.com>
2020-08-03 20:59:17 +0530

    1.Login page and yami page styling 2.commented footer for time being 3.currently working on tabs

--- Backend/Controllers/dbm.js
+++ Backend/Controllers/dbm.js
@@ -1,107 +1,117 @@
-var mongoose = require('mongoose');
-var User = require('../models/User');
-var bcrypt = require('bcrypt');
-var fs = require('fs');
-require('dotenv').config();
-var passport  = require('passport');
-var localStrategy = require('passport-local').Strategy;
+var mongoose = require("mongoose");
+var User = require("../models/User");
+var bcrypt = require("bcrypt");
+var fs = require("fs");
+require("dotenv").config();
+var passport = require("passport");
+var localStrategy = require("passport-local").Strategy;
 
-// internal guide => 
+// internal guide =>
 
-mongoose.connect(process.env.uri,{
-	useNewUrlParser : true,
-	useUnifiedTopology: true
-	},function(err){
-	if (err){
-		console.log(err);
-	}else{
-		console.log("Connected to database");
-		// CUSTOM CHANGE TO DATABASE HERE 
-		/*
+mongoose.connect(
+  process.env.uri,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Connected to database");
+      // CUSTOM CHANGE TO DATABASE HERE
+      /*
 		User.find({type:'student'},function(err,data){
 			for (i = 0 ; i < data.length ; i++){
 				console.log(data[i].email);
 			}
 		});*/
-	}
-});
+    }
+  }
+);
 
 function makePassword(length) {
-	var result = '';
-	var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	var charactersLength = characters.length;
-	for ( var i = 0; i < length; i++ ) {
-	  result += characters.charAt(Math.floor(Math.random() * charactersLength));
-	}
-	return result;
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
-function changePassword(user,newPassword){
-	return User.findById(user.id, function(err,user){
-		if (err) return false;
-		return bcrypt.hash(newPassword,10,function(err,hash){
-			if (err) return false;
-			user.password = hash;
-			return user.save(function(err,user){
-				if (err) return false;
-				else return true;
-			});
-		});
-	})
-	
-}
-
-function saveLocallyForDevelopment(email,password){
-	line = email+","+password +"\n";
-	fs.appendFile('credentials.txt',line,function(err){
-		if(err) throw err;
-	})
+function changePassword(user, newPassword) {
+  return User.findById(user.id, function (err, user) {
+    if (err) return false;
+    return bcrypt.hash(newPassword, 10, function (err, hash) {
+      if (err) return false;
+      user.password = hash;
+      return user.save(function (err, user) {
+        if (err) return false;
+        else return true;
+      });
+    });
+  });
 }
 
-function addToDatabase(admin,email,department,type,projectName=null){
-	password = makePassword(8);
-	saveLocallyForDevelopment(email,password);
-	bcrypt.hash(password,10,function(err,hash){
-		if (err) throw err;
-		var user  = User();
-		user.email = email;
-		user.password = hash;
-		user.department = department;
-		user.type = type;
-		user.admin_id = admin.id;
-		if (projectName){
-			user.projectName = projectName;
-		}
-		user.save(function(err,user){
-			if (err) throw err;
-			else console.log("Added ",user.email);
-
-		});
-		
-	});	
+function saveLocallyForDevelopment(email, password) {
+  line = email + "," + password + "\n";
+  fs.appendFile("credentials.txt", line, function (err) {
+    if (err) throw err;
+  });
 }
 
-passport.use(new localStrategy({usernameField:'email'},function(email,password,done){
-	User.findOne({email:email},function(err,user){
-		if (err) return done(err);
-		if (user == null) return done(null,false,{message:"No User With That Email"});
-		bcrypt.compare(password,user.password,function(err,result){
-			if (result) return done(null,user,{message:"Successfully Logged In"});	
-			else return done(null,false,{message:"Invalid password"});
-		});
-	});
-}));
-passport.serializeUser(function(user,done){
-	done(null,user.id)
+function addToDatabase(admin, email, department, type, projectName = null) {
+  password = makePassword(8);
+  saveLocallyForDevelopment(email, password);
+  bcrypt.hash(password, 10, function (err, hash) {
+    if (err) throw err;
+    var user = User();
+    user.email = email;
+    user.password = hash;
+    user.department = department;
+    user.type = type;
+    user.admin_id = admin.id;
+    if (projectName) {
+      user.projectName = projectName;
+    }
+    user.save(function (err, user) {
+      if (err) throw err;
+      else console.log("Added ", user.email);
+    });
+  });
+}
+
+passport.use(
+  new localStrategy({ usernameField: "email" }, function (
+    email,
+    password,
+    done
+  ) {
+    User.findOne({ email: email }, function (err, user) {
+      if (err) return done(err);
+      if (user == null)
+        return done(null, false, { message: "No User With That Email" });
+      bcrypt.compare(password, user.password, function (err, result) {
+        if (result)
+          return done(null, user, { message: "Successfully Logged In" });
+        else return done(null, false, { message: "Invalid password" });
+      });
+    });
+  })
+);
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
 });
-passport.deserializeUser(function(id,done){
-	User.findById(id,function(err,user){
-		if (err) return done(err);
-		else return done(null,user);
-	});
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    if (err) return done(err);
+    else return done(null, user);
+  });
 });
 
 module.exports = {
-	addToDatabase : addToDatabase,	
-	passport : passport ,
-	changePassword : changePassword,
-}
+  addToDatabase: addToDatabase,
+  passport: passport,
+  changePassword: changePassword
+};
